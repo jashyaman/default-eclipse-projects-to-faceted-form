88a12412fbf92842a1c2f0c37b82ed62b9ba7977
Shyam Raman <shyraman@deloitte.com>
2019-01-18 22:55:01 -0500

    refactored.tested-OK-

--- src/test/Test.java
+++ src/test/Test.java
@@ -17,17 +17,13 @@ import java.util.List;
 
 public class Test {
 
-	
-	//private static final String testdir = "C:\\Users\\shyraman\\workspace\\testdir";
 	private static final String dir = ".settings";
 	private static final String testdir = "C:\\Users\\shyraman\\workspace\\mvdls\\";
-	//private static final String dir = "settings";
 
 
 	public static void main(String[] args) {
 		
 		try  {
-			//File location = new File("C:\\Users\\shyraman\\workspace\\mvdls\\") ;
 			File location = new File(testdir);
 			System.out.println("start");
 			    
@@ -39,15 +35,13 @@ public class Test {
 					System.out.println("check");
 				}
 				
-				String fileName = "org.eclipse.wst.common.project.facet.core";
-				String ext = "xml";
+				String fileName = "org.eclipse.wst.common.project.facet.core.xml";
 
 				String contents = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n" + 
 						"<faceted-project>\r\n" + 
 						"  <installed facet=\"java\" version=\"1.8\"/>\r\n" + 
 						"</faceted-project>\r\n" + 
 						"";
-				
 				
 				String fileName2 = "org.eclipse.jdt.core.prefs";
 				String contents2 = "eclipse.preferences.version=1\r\n" + 
@@ -91,57 +85,79 @@ public class Test {
 						"</projectDescription>\r\n" + 
 						"";
 				
+				String filename4 = ".classpath";
+				String contents4 = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n" + 
+						"<classpath>\r\n" + 
+						"	<classpathentry kind=\"src\" path=\"src/main/java\"/>\r\n" + 
+						"	<classpathentry kind=\"con\" path=\"org.eclipse.jdt.launching.JRE_CONTAINER/org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType/JavaSE-1.8\">\r\n" + 
+						"		<attributes>\r\n" + 
+						"			<attribute name=\"owner.project.facets\" value=\"java\"/>\r\n" + 
+						"		</attributes>\r\n" + 
+						"	</classpathentry>\r\n" + 
+						"	<classpathentry kind=\"output\" path=\"bin\"/>\r\n" + 
+						"</classpath>\r\n" + 
+						"";
+				
 				for(String s : reqdDir) {
 					
 					File f = new File(s);
-					
-					
 					if(f.isDirectory()) {
-						//System.out.println(f.getParentFile().getName());
 						String newcontents3 = contents3.replaceAll("INSERT_NAME_HERE", f.getParentFile().getName());
-						//System.out.println("replaced");
-						//System.out.println(f.getAbsolutePath()+"\\"+f.list()[0]);
-						//new File(f.getAbsolutePath()+"\\"+f.list()[0]).delete();
+						File eclipseProjectConfigFile = new File(f.getParentFile().getAbsolutePath()+"\\"+fileName3);
+						File eclipseCorePrefs = new File(f.getAbsolutePath()+"\\"+fileName2);
+						File eclipseFacetFile = new File(f.getAbsolutePath()+"\\"+fileName);
 						
-						//File fCheck = new File(f.getAbsolutePath()+"\\"+fileName+"."+ext);
-						//File fCheck = new File(f.getAbsolutePath()+"\\"+fileName2);
-						File fCheck = new File(f.getParentFile().getAbsolutePath()+"\\"+fileName3);
-						if(fCheck.exists() && !fCheck.isDirectory()) { 
-						    fCheck.delete();
-						    Files.write(Paths.get(f.getParentFile().getAbsolutePath()+"\\"+fileName3), newcontents3.getBytes());
-						    
-							//System.out.println(f.getParentFile().getAbsolutePath()+"\\"+fileName3);
-							//System.out.println(newcontents3);
+						File projectClasspathFile = new File(f.getParentFile().getAbsolutePath()+"\\"+filename4);
+						
+						if(projectClasspathFile.exists() && !projectClasspathFile.isDirectory()) {
+							//projectClasspathFile.delete();
+							//Files.write(Paths.get(f.getParentFile().getAbsolutePath()+"\\"+filename4), contents4.getBytes());
 						} else {
-							//Files.write(Paths.get(f.getAbsolutePath()+"\\"+fileName+"."+ext), contents.getBytes());
-						//	Files.write(Paths.get(f.getAbsolutePath()+"\\"+fileName2), contents2.getBytes());
+							Files.write(Paths.get(f.getParentFile().getAbsolutePath()+"\\"+filename4), contents4.getBytes());
 						}
+						
+						if(eclipseCorePrefs.exists() && !eclipseCorePrefs.isDirectory()) {
+							//eclipseCorePrefs.delete();
+							//Files.write(Paths.get(f.getAbsolutePath()+"\\"+fileName2), contents2.getBytes());
+						} else {
+							Files.write(Paths.get(f.getAbsolutePath()+"\\"+fileName2), contents2.getBytes());
+						}
+						
+						if(eclipseFacetFile.exists() && !eclipseFacetFile.isDirectory()) {
+							//eclipseFacetFile.delete();
+							//Files.write(Paths.get(f.getAbsolutePath()+"\\"+fileName), contents.getBytes());
+						} else {
+							Files.write(Paths.get(f.getAbsolutePath()+"\\"+fileName), contents.getBytes());
+						}
+						
+						if(eclipseProjectConfigFile.exists() && !eclipseProjectConfigFile.isDirectory()) { 
+						   // eclipseProjectConfigFile.delete();
+						    //Files.write(Paths.get(f.getParentFile().getAbsolutePath()+"\\"+fileName3), newcontents3.getBytes());
+						} else {
+							Files.write(Paths.get(f.getParentFile().getAbsolutePath()+"\\"+fileName3), newcontents3.getBytes());
+						}
+						
+						
 					}
 				}
 			
 		} catch(NullPointerException e) {
 			System.err.println("error : location is null " + e.getMessage());
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.err.println("error : Issue with writing to file " + e.getMessage());
 		} 
 		
 		System.out.println("end");
 	}
 
 	private static List<String> callSubDir(File location,List<String> arr) {
-		
 		if(arr == null) {
 			arr = new ArrayList<String>();
 		}
-		
 		if(location.isFile()) {
 			return arr;
 		}
-		
-		
 		if(location.isDirectory()) {
-			
 			if(location.getName().equalsIgnoreCase(dir)) {
 				arr.add(location.getAbsolutePath());
 			} else {
